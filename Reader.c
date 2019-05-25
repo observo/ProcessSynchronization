da60965e97389d5a82a24405e7802da4566815a5
observo <observo.code@gmail.com>
2019-05-25 18:38:26 +0600

    Process Synchronization Working.

--- Reader.c
+++ Reader.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char* argv[]){
     if(argc==2){
-        char FileName[128]=argv[1];
+        char* FileName=argv[1];
         struct MemData{
             sem_t FullMutex;
             sem_t FreeMutex;
